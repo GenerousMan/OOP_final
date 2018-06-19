c5e2a173a41a6f60af12d2a24e8a13af87c8c0a7
JiangWenPL <JiangWenPL@users.noreply.github.com>
2018-06-19 13:46:27 +0800

    to_Qimage and from QImage done.

--- MeiTuShop/MeiTuShop/ImageProcess.h
+++ MeiTuShop/MeiTuShop/ImageProcess.h
@@ -10,29 +10,28 @@
 #include <utility>
 #include <string>
 #include <exception>
+#include <QtGui/QImage>
 
 class ImageProcessor {
 public:
     ImageProcessor() = default;
 
-    ImageProcessor(const cv::Mat &img) {
+    explicit ImageProcessor(const cv::Mat &img) {
         m_img = img.clone();
     }
 
-    ImageProcessor &operator=(const ImageProcessor &img_pro) {
-        this->m_img = img_pro.m_img.clone();
-        return *this;
-    }
+    ImageProcessor(QImage const &img, unsigned int format = CV_8UC3);
 
-    ImageProcessor &operator=(const cv::Mat &img) {
-        m_img = img.clone();
-        return *this;
-    }
+    ImageProcessor &operator=(QImage const &img);
+
+    ImageProcessor &operator=(const ImageProcessor &img_pro);
+
+    ImageProcessor &operator=(const cv::Mat &img);
+
+    explicit operator QImage() const;
 
     //For debug convenience
-    explicit operator cv::Mat() const {
-        return m_img.clone();
-    }
+    explicit operator cv::Mat() const;
 
     //For debug convenience
     void imshow(const std::string &name = "test") const;
@@ -44,7 +43,10 @@ public:
 
     ImageProcessor crop(cv::Rect rect);
 
+    QImage to_QImage(QImage::Format format = QImage::Format_RGB888) const;
+
     ~ImageProcessor() = default;
+
 
 private:
     cv::Mat m_img;
@@ -96,10 +98,46 @@ ImageProcessor ImageProcessor::crop(cv::Rect rect) {
     rect = rect & srcRect;
     if (rect.width <= 0 || rect.height <= 0)
         throw std::runtime_error("witdh and height should > 0");
-
     croped_img.m_img.create(cv::Size(rect.width, rect.height), m_img.type());
     m_img(rect).copyTo(croped_img.m_img);
     return croped_img;
+}
+
+QImage ImageProcessor::to_QImage(QImage::Format format) const {
+    cv::Mat temp_img;
+    cv::cvtColor(m_img, temp_img, cv::COLOR_BGR2RGB);
+    return QImage(temp_img.data, temp_img.cols, temp_img.rows,
+                  static_cast<int>(temp_img.step), format).copy();
+}
+
+ImageProcessor::operator QImage() const {
+    return this->to_QImage();
+}
+
+ImageProcessor::operator cv::Mat() const {
+    return m_img.clone();
+}
+
+ImageProcessor::ImageProcessor(QImage const &img, unsigned int format) {
+    auto temp_img = cv::Mat(img.height(), img.width(), format, const_cast<uchar *>(img.bits()),
+                            static_cast<size_t>(img.bytesPerLine())).clone();
+    cv::cvtColor(temp_img, m_img, CV_RGB2BGR);
+}
+
+ImageProcessor &ImageProcessor::operator=(QImage const &img) {
+    this->m_img = cv::Mat(img.height(), img.width(), CV_8UC3, const_cast<uchar *>(img.bits()),
+                          static_cast<size_t>(img.bytesPerLine())).clone();
+    return *this;
+}
+
+ImageProcessor &ImageProcessor::operator=(const ImageProcessor &img_pro) {
+    this->m_img = img_pro.m_img.clone();
+    return *this;
+}
+
+ImageProcessor &ImageProcessor::operator=(const cv::Mat &img) {
+    m_img = img.clone();
+    return *this;
 }
 
 #endif //OOP_FINAL_IMAGEPROCESS_H
--- MeiTuShop/MeiTuShop/image_test.cpp
+++ MeiTuShop/MeiTuShop/image_test.cpp
@@ -2,11 +2,16 @@
 // Created by Wen Jiang on 6/19/18.
 //
 #include "ImageProcess.h"
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
 
-int main() {
+int main(int argc, char *argv[]) {
     ImageProcessor img = cv::imread("../test_image/lena.bmp");
-    img.imshow();
     auto r_img = img.crop(cv::Rect(0, 0, 256, 256));
-    r_img.imshow();
-    return 0;
+    QApplication a(argc, argv);
+    QLabel *l = new QLabel();
+    l->setPixmap(QPixmap::fromImage(img.to_QImage()));
+    l->show();
+    return a.exec();
 }
