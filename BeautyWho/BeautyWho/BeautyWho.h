72e2dbf2154259665b15d68e6f2d859cc155d8dc
Yang Yuhao <3160105521@zju.edu.cn>
2018-06-20 20:27:28 +0800

    大改了一下，现在打开图片已经完成了
    而且实现了能打开任意尺寸的图片，添加了滚动条。

---
+++ BeautyWho/BeautyWho/BeautyWho.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_BeautyWho.h"
+#include "opencv2/opencv.hpp"
+#include ".\Backend\ImageProcess.h"
+using namespace cv;
+
+class BeautyWho : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	BeautyWho(QWidget *parent = Q_NULLPTR);
+
+private slots:
+	void open_pic();
+
+private:
+	Ui::BeautyWhoClass *ui;
+	QLabel *label;
+};
--- BeautyWho/BeautyWho/main.cpp
+++ BeautyWho/BeautyWho/main.cpp
@@ -1,10 +1,10 @@
-#include "MeiTuShop.h"
+#include "BeautyWho.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MeiTuShop w;
+	BeautyWho w;
 	w.show();
 	return a.exec();
 }
