e8eed503f2f093d19513b64f0899ec8a794874e0
lthrosengun <lthrosengun@gmail.com>
2018-06-25 13:17:07 +0800

    根据事件修改标签内容

--- BeautyWho/BeautyWho/BeautyWho.cpp
+++ BeautyWho/BeautyWho/BeautyWho.cpp
@@ -4,18 +4,21 @@
 #include"./Backend/ImageProcess.h"
 #include <string>
 
-enum{OPEN_FILE,GRAY,WHITE,CHANGE_LIGHT,ROTATE,NOTHING};// current event
+enum { OPEN_FILE, GRAY, WHITE, CHANGE_LIGHT, ROTATE, WHITE_BALANCE, NOTHING };// current event
 ImageProcessor img;//original picture
-bool opened_file = false; 
-int curr_event=NOTHING;
+bool opened_file = false;
+int curr_event = NOTHING;
 
 BeautyWho::BeautyWho(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui->setupUi(this);
+	ui->dockWidget->setMinimumSize(300, 300);
+	ui->dockWidget->setMaximumSize(300, 300);
 }
 
 void BeautyWho::open_clicked() {
+	ui->label_3->setText("");
 	if (curr_event != NOTHING) {
 		QLabel *label = new QLabel();
 		ui->picafter->setWidget(label);
@@ -49,7 +52,7 @@ void BeautyWho::show_image(QImage *img, bool pic)const {
 		ui->picbefore->setWidget(label);
 }
 
-bool BeautyWho::judge()  {
+bool BeautyWho::judge() {
 	if (!opened_file) {
 		QMessageBox::information(this,
 			tr("Wanning"),
@@ -60,6 +63,7 @@ bool BeautyWho::judge()  {
 }
 
 void BeautyWho::gray_clicked() {
+	ui->label_3->setText("");
 	curr_event = GRAY;
 	if (judge()) {
 		ImageProcessor n_img = img;
@@ -69,11 +73,14 @@ void BeautyWho::gray_clicked() {
 }
 
 void BeautyWho::white_clicked() {
-	curr_event = WHITE; 
+	ui->label_3->setText(tr("变白程度(1-30)"));
+	curr_event = WHITE;
 	ui->horizontalSlider->setRange(1, 30);
 }
 
 void BeautyWho::white_balance_clicked() {
+	ui->label_3->setText("");
+	curr_event = WHITE_BALANCE;
 	if (judge()) {
 		ImageProcessor n_img = img;
 		auto nn_img = n_img.white_balance();
@@ -82,34 +89,36 @@ void BeautyWho::white_balance_clicked() {
 }
 
 void BeautyWho::bright_clicked() {
+	ui->label_3->setText(tr("调节亮度(0-255)"));
 	curr_event = CHANGE_LIGHT;
 	ui->horizontalSlider->setRange(0, 255);
 }
 
 void BeautyWho::rotate_clicked() {
+	ui->label_3->setText(tr("旋转角度(0-359)"));
 	curr_event = ROTATE;
 	ui->horizontalSlider->setRange(0, 359);
 }
 void BeautyWho::onSliderValueChanged(int i) {
 	switch (curr_event) {
-		case CHANGE_LIGHT:
-			change_light(i);
-			break;
-		case ROTATE:
-			rotate(i);
-			break;
-		case WHITE:
-			white((i+100)/100.0);
-			break;
-		default:
-			break;
+	case CHANGE_LIGHT:
+		change_light(i);
+		break;
+	case ROTATE:
+		rotate(i);
+		break;
+	case WHITE:
+		white((i + 100) / 100.0);
+		break;
+	default:
+		break;
 	}
 }
-void BeautyWho::change_light(int beta){
-	if (judge()){
+void BeautyWho::change_light(int beta) {
+	if (judge()) {
 		ImageProcessor n_img = img;
 		auto nn_img = n_img.light(beta);
-		show_image(&(nn_img.to_QImage()),1); 
+		show_image(&(nn_img.to_QImage()), 1);
 	}
 }
 
